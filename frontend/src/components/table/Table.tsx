5bfc377239ef7c9eea205b5f6eaa8bb810745c50
Purva Chakravarti <purvachakravarti@gmail.com>
2026-02-27 15:52:18 -0800

    Removed the extra space created by tfoot

--- frontend/src/components/table/Table.tsx
+++ frontend/src/components/table/Table.tsx
@@ -188,29 +188,6 @@ const TableContent = memo(() => {
             </tr>
           ))}
         </tbody>
-
-        <tfoot>
-          {table.getFooterGroups().map((footerGroup) => (
-            <tr
-              className="bg-gray-900/40 hover:bg-gray-900/70 transition-colors"
-              key={footerGroup.id}
-            >
-              {footerGroup.headers.map((header) => (
-                <th
-                  className="px-4 py-3 text-left text-sm font-semibold text-gray-200"
-                  key={header.id}
-                >
-                  {header.isPlaceholder
-                    ? null
-                    : flexRender(
-                        header.column.columnDef.footer,
-                        header.getContext(),
-                      )}
-                </th>
-              ))}
-            </tr>
-          ))}
-        </tfoot>
       </table>
     </div>
   );
